b4433c0849244ba5c87ca1739a0e2d598be688d1
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2024-02-25 14:27:07 +0700

    chore: vertical camera control - wip

--- src/systems/CameraFollow.tsx
+++ src/systems/CameraFollow.tsx
@@ -1,11 +1,23 @@
 import { useFrame } from '@react-three/fiber'
 
+import { DEFAULT_CAMERA_ROT_X } from '#/lib/constants'
 import { lerp, pi } from '#/lib/util'
+import useStore from '#/store'
 import { cameras, players } from '#/world'
 
 const Z_OFFSET = 3
 const DISTANCE_FROM_PLAYER = 4
-const VERTICAL_ANGLE = -0.35
+const HEIGHT_ADJUSTMENT_FACTOR = 2
+const DISTANCE_ADJUSTMENT_FACTOR = -3
+
+const adjustHeightBasedOnPitch = (pitch: number, baseHeight: number, adjustmentFactor: number) =>
+  baseHeight + Math.sin(-pitch) * adjustmentFactor
+
+const adjustDistanceBasedOnPitch = (
+  pitch: number,
+  baseDistance: number,
+  adjustmentFactor: number,
+) => baseDistance + Math.sin(-pitch) * adjustmentFactor
 
 const CameraFollowSystem = () => {
   useFrame(() => {
@@ -16,6 +28,42 @@ const CameraFollowSystem = () => {
       return
     }
 
+    camera.tra.rot.z = lerp(camera.tra.rot.z, player.tra.rot.z, 0.5) - pi / 4
+    camera.tra.rot.y = lerp(camera.tra.rot.y!, 0, 0.1)
+
+    if (useStore.getState().inputs.pointerLock) {
+      // if (useStore.getState().controls.manualRotX) {
+      //   camera.tra.rot.x! = lerp(
+      //     camera.tra.rot.x!,
+      //     camera.tra.rot.x! + useStore.getState().controls.manualRotX!,
+      //     0.1,
+      //   )
+      // }
+      // const adjustedZOffset = adjustHeightBasedOnPitch(
+      //   camera.tra.rot.x!,
+      //   Z_OFFSET,
+      //   HEIGHT_ADJUSTMENT_FACTOR,
+      // )
+      // const adjustedDistanceFromPlayer = adjustDistanceBasedOnPitch(
+      //   camera.tra.rot.x!,
+      //   DISTANCE_FROM_PLAYER,
+      //   DISTANCE_ADJUSTMENT_FACTOR,
+      // )
+      // camera.tra.pos.z = lerp(camera.tra.pos.z!, adjustedZOffset, 0.1)
+      // camera.tra.pos.x = lerp(
+      //   camera.tra.pos.x,
+      //   player.tra.pos.x - Math.cos(player.tra.rot.z) * adjustedDistanceFromPlayer,
+      //   0.5,
+      // )
+      // camera.tra.pos.y = lerp(
+      //   camera.tra.pos.y,
+      //   player.tra.pos.y - Math.sin(player.tra.rot.z) * adjustedDistanceFromPlayer,
+      //   0.5,
+      // )
+    } else {
+    }
+    camera.tra.rot.x = lerp(camera.tra.rot.x!, DEFAULT_CAMERA_ROT_X, 0.1)
+    camera.tra.pos.z = lerp(camera.tra.pos.z!, Z_OFFSET, 0.1)
     camera.tra.pos.x = lerp(
       camera.tra.pos.x,
       player.tra.pos.x - Math.cos(player.tra.rot.z) * DISTANCE_FROM_PLAYER,
@@ -27,11 +75,6 @@ const CameraFollowSystem = () => {
       player.tra.pos.y - Math.sin(player.tra.rot.z) * DISTANCE_FROM_PLAYER,
       0.5,
     )
-
-    camera.tra.pos.z = lerp(camera.tra.pos.z!, Z_OFFSET, 0.1)
-    camera.tra.rot.z = lerp(camera.tra.rot.z, player.tra.rot.z, 0.5) - pi / 4
-    camera.tra.rot.x = lerp(camera.tra.rot.x!, VERTICAL_ANGLE, 0.1)
-    camera.tra.rot.y = lerp(camera.tra.rot.y!, 0, 0.1)
   })
 
   return null
