00dfdbd94ef6f7a1db3ee9f8672caa9fcf42674a
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2024-03-16 15:26:41 +0700

    feat: right joystick camera rotation

--- src/systems/ControlsSystem.tsx
+++ src/systems/ControlsSystem.tsx
@@ -8,52 +8,95 @@ import { jump, uTurn } from '#/systems/MovementSystem'
 
 const MAX_MOVEMENT = 60
 
-const joystickForward = () => {
+const mobileJoystickUp = () => {
   const { movementMobileJoystick } = mp()
 
-  if (movementMobileJoystick.angle === undefined || movementMobileJoystick.force === undefined) return false
+  if (movementMobileJoystick.angle === undefined || movementMobileJoystick.force === undefined)
+    return false
 
-  return movementMobileJoystick.force > 0.2 && between(movementMobileJoystick.angle, pi / 4, (3 * pi) / 4)
+  return (
+    movementMobileJoystick.force > 0.2 &&
+    between(movementMobileJoystick.angle, pi / 4, (3 * pi) / 4)
+  )
 }
 
-const joystickBackward = () => {
+const mobileJoystickDown = () => {
   const { movementMobileJoystick } = mp()
 
-  if (movementMobileJoystick.angle === undefined || movementMobileJoystick.force === undefined) return false
+  if (movementMobileJoystick.angle === undefined || movementMobileJoystick.force === undefined)
+    return false
 
-  return movementMobileJoystick.force > 0.2 && between(movementMobileJoystick.angle, (4 * pi) / 3, (5 * pi) / 3)
+  return (
+    movementMobileJoystick.force > 0.2 &&
+    between(movementMobileJoystick.angle, (4 * pi) / 3, (5 * pi) / 3)
+  )
 }
 
-const joystickStrafeLeft = () => {
+const mobileJoystickLeft = () => {
   const { movementMobileJoystick } = mp()
 
-  if (movementMobileJoystick.angle === undefined || movementMobileJoystick.force === undefined) return false
+  if (movementMobileJoystick.angle === undefined || movementMobileJoystick.force === undefined)
+    return false
 
-  return movementMobileJoystick.force > 0.2 && between(movementMobileJoystick.angle, pi, (7 * pi) / 6)
+  return (
+    movementMobileJoystick.force > 0.2 && between(movementMobileJoystick.angle, pi, (7 * pi) / 6)
+  )
 }
 
-const joystickStrafeRight = () => {
+const mobileJoystickRight = () => {
   const { movementMobileJoystick } = mp()
 
-  if (movementMobileJoystick.angle === undefined || movementMobileJoystick.force === undefined) return false
+  if (movementMobileJoystick.angle === undefined || movementMobileJoystick.force === undefined)
+    return false
 
-  return movementMobileJoystick.force > 0.2 && between(movementMobileJoystick.angle, (11 * pi) / 6, 2 * pi)
+  return (
+    movementMobileJoystick.force > 0.2 &&
+    between(movementMobileJoystick.angle, (11 * pi) / 6, 2 * pi)
+  )
 }
 
-const joystickBackwardLeft = () => {
+const mobileJoystickDownLeft = () => {
   const { movementMobileJoystick } = mp()
 
-  if (movementMobileJoystick.angle === undefined || movementMobileJoystick.force === undefined) return false
+  if (movementMobileJoystick.angle === undefined || movementMobileJoystick.force === undefined)
+    return false
 
-  return movementMobileJoystick.force > 0.2 && between(movementMobileJoystick.angle, (7 * pi) / 6, (4 * pi) / 3)
+  return (
+    movementMobileJoystick.force > 0.2 &&
+    between(movementMobileJoystick.angle, (7 * pi) / 6, (4 * pi) / 3)
+  )
 }
 
-const joystickBackwardRight = () => {
+const mobileJoystickDownRight = () => {
   const { movementMobileJoystick } = mp()
 
-  if (movementMobileJoystick.angle === undefined || movementMobileJoystick.force === undefined) return false
+  if (movementMobileJoystick.angle === undefined || movementMobileJoystick.force === undefined)
+    return false
 
-  return movementMobileJoystick.force > 0.2 && between(movementMobileJoystick.angle, (5 * pi) / 3, (11 * pi) / 6)
+  return (
+    movementMobileJoystick.force > 0.2 &&
+    between(movementMobileJoystick.angle, (5 * pi) / 3, (11 * pi) / 6)
+  )
+}
+
+const mobileJoystickUpLeft = () => {
+  const { movementMobileJoystick } = mp()
+
+  if (movementMobileJoystick.angle === undefined || movementMobileJoystick.force === undefined)
+    return false
+
+  return (
+    movementMobileJoystick.force > 0.2 && between(movementMobileJoystick.angle, (3 * pi) / 4, pi)
+  )
+}
+
+const mobileJoystickUpRight = () => {
+  const { movementMobileJoystick } = mp()
+
+  if (movementMobileJoystick.angle === undefined || movementMobileJoystick.force === undefined)
+    return false
+
+  return movementMobileJoystick.force > 0.2 && between(movementMobileJoystick.angle, 0, pi / 4)
 }
 
 const getForward = () => {
@@ -69,7 +112,7 @@ const getForward = () => {
 
   if (k.KeyW || k.ArrowUp) return true
   if (isLeftMouseDown && isRightMouseDown) return true
-  if (joystickForward()) return true
+  if (mobileJoystickUp()) return true
 
   return false
 }
@@ -85,7 +128,7 @@ const getBackward = () => {
   if (isPointerLocked && (k.KeyA || k.ArrowLeft)) return false
   if (isPointerLocked && (k.KeyD || k.ArrowRight)) return false
   if (k.KeyS || k.ArrowDown) return true
-  if (joystickBackward()) return true
+  if (mobileJoystickDown()) return true
 
   return false
 }
@@ -123,7 +166,7 @@ const getStrafeLeft = () => {
   if (isLeftMouseDown && isRightMouseDown) return false
   if (k.KeyQ) return true
   if (isPointerLocked && (k.KeyA || k.ArrowLeft)) return true
-  if (joystickStrafeLeft()) return true
+  if (mobileJoystickLeft()) return true
 
   return false
 }
@@ -139,7 +182,7 @@ const getStrafeRight = () => {
   if (isLeftMouseDown && isRightMouseDown) return false
   if (k.KeyE) return true
   if (isPointerLocked && (k.KeyD || k.ArrowRight)) return true
-  if (joystickStrafeRight()) return true
+  if (mobileJoystickRight()) return true
 
   return false
 }
@@ -152,6 +195,7 @@ const getForwardLeft = () => {
   if (isPointerLocked && ((k.KeyA && k.KeyW) || (k.ArrowUp && k.ArrowLeft))) return true
   if (isLeftMouseDown && isRightMouseDown && k.KeyQ) return true
   if (isLeftMouseDown && isRightMouseDown && (k.ArrowLeft || k.KeyA)) return true
+  if (mobileJoystickUpLeft()) return true
 
   return false
 }
@@ -164,6 +208,7 @@ const getForwardRight = () => {
   if (isPointerLocked && ((k.KeyD && k.KeyW) || (k.ArrowUp && k.ArrowRight))) return true
   if (isLeftMouseDown && isRightMouseDown && k.KeyE) return true
   if (isLeftMouseDown && isRightMouseDown && (k.ArrowRight || k.KeyD)) return true
+  if (mobileJoystickUpRight()) return true
 
   return false
 }
@@ -174,7 +219,7 @@ const getBackwardLeft = () => {
 
   if (k.KeyQ && k.KeyS) return true
   if (isPointerLocked && ((k.KeyA && k.KeyS) || (k.ArrowDown && k.ArrowLeft))) return true
-  if (joystickBackwardLeft()) return true
+  if (mobileJoystickDownLeft()) return true
 
   return false
 }
@@ -185,7 +230,7 @@ const getBackwardRight = () => {
 
   if (k.KeyE && k.KeyS) return true
   if (isPointerLocked && ((k.KeyD && k.KeyS) || (k.ArrowDown && k.ArrowRight))) return true
-  if (joystickBackwardRight()) return true
+  if (mobileJoystickDownRight()) return true
 
   return false
 }
@@ -195,7 +240,7 @@ const getBackwardRight = () => {
 
 const ControlsSystem = () => {
   useFrame(() => {
-    const { movementMobileJoystick, keys } = mp()
+    const { cameraMobileJoystick, keys } = mp()
     const { isPointerLocked, mouseMovementX, mouseMovementY } = mp()
 
     setControl('forward', getForward())
@@ -218,15 +263,13 @@ const ControlsSystem = () => {
         setControl('manualRotX', clamp(mouseMovementY, MAX_MOVEMENT) / 100)
       }
     } else if (
-      movementMobileJoystick.force !== undefined &&
-      movementMobileJoystick.angle !== undefined &&
-      movementMobileJoystick.force > 0.2
+      cameraMobileJoystick.force !== undefined &&
+      cameraMobileJoystick.angle !== undefined
     ) {
-      if (between(movementMobileJoystick.angle, 0, pi)) {
-        setControl('manualRotZ', (movementMobileJoystick.angle - pi / 2) * 0.1)
-      } else {
-        setControl('manualRotZ', undefined)
-      }
+      setControl(
+        'manualRotZ',
+        -Math.cos(cameraMobileJoystick.angle) * 0.1 * Math.min(cameraMobileJoystick.force, 2),
+      )
     } else {
       setControl('manualRotZ', undefined)
       setControl('manualRotX', undefined)
