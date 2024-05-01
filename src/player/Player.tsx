9559a678f86c3c408b7c60ed266c0825ca451ee4
verekia <522007+verekia@users.noreply.github.com>
2024-05-01 10:28:06 +0700

    docs: add comment about managing animations

--- src/player/Player.tsx
+++ src/player/Player.tsx
@@ -2,6 +2,9 @@ import { ECS, Entity } from '#/ecs/world'
 import PlayerModel from '#/player/PlayerModel'
 
 const Player = ({ player }: Entity) => {
+  // Alternatively, watch for changes in a player.animation non-reactive value
+  // That worked well for the version version of the game and is probably not
+  // that expensive. This lets me keep the world and systems Zustand-free.
   const animation = player?.usePlayerStore((state: any) => state.animation)
   const modelRotZ = player?.usePlayerStore((state: any) => state.modelRotZ)
 
